a5094d268a393209162d47367fff333b0acf0abc
erickrex <59777190+erickrex@users.noreply.github.com>
2021-01-08 13:36:33 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -31,7 +31,7 @@ function Navbar() {
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
             Erick REX
-            <i class='fab fa-id-card' />
+            
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
