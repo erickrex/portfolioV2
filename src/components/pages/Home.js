ba886ecce1d9aae77c782ea8e0f76e2cfd5f4c7a
erickrex <59777190+erickrex@users.noreply.github.com>
2021-01-20 15:16:38 +0100

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,10 +3,8 @@ import '../../App.css';
 import Cards from '../Cards';
 import HeroSection from '../HeroSection';
 import Footer from '../Footer';
-//import CardLeft from '../CardLeft'
 import Projects from '../Projects'
 import Center from '../Center'
-import 'aos/dist/aos.css';
 
 function Home() {
 
